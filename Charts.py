d389c3ef97cdbca0f37fd1fb2a5d539886a131db
adennguyen21 <adennguyen21@gmail.com>
2026-03-30 11:27:47 -0600

    Create dictionaries for each of the evaluation categories.

---
+++ Charts.py
@@ -0,0 +1,8 @@
+import matplotlib as plt
+
+accuracy_dictionary = {"Logistic Regression": 0.880, "KNN": 0.899, "Decision Tree": 0.841, "Neural Network": 0.870}
+precision_dictionary = {"Logistic Regression": 0.917, "KNN": 0.925, "Decision Tree": 0.857, "Neural Network": 0.905}
+recall_dictionary = {"Logistic Regression": 0.878, "KNN": 0.902, "Decision Tree": 0.878, "Neural Network": 0.872}
+f1_dictionary = {"Logistic Regression": 0.897, "KNN": 0.914, "Decision Tree": 0.867, "Neural Network": 0.888}
+
+
