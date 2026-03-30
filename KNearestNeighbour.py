425520e5c7c565108678c1ea922ba674e93c8d37
adennguyen21 <adennguyen21@gmail.com>
2026-03-30 02:46:35 -0600

    Increase number of k values.

--- KNearestNeighbour.py
+++ KNearestNeighbour.py
@@ -32,7 +32,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Create KNN model
-number_of_neighbors = [1,2,3,4,5]
+number_of_neighbors = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 for k in number_of_neighbors:
     knn_model = KNeighborsClassifier(n_neighbors=k)
 
