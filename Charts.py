fbb32e103ef31c95f823c7fc927d20c19f7c69d4
adennguyen21 <adennguyen21@gmail.com>
2026-04-01 01:13:07 -0600

    Change to updated values for the dictionaries.

--- Charts.py
+++ Charts.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-accuracy_dictionary = {"Logistic Regression": 0.880, "KNN": 0.899, "Decision Tree": 0.841, "Neural Network": 0.870}
-precision_dictionary = {"Logistic Regression": 0.917, "KNN": 0.925, "Decision Tree": 0.857, "Neural Network": 0.905}
-recall_dictionary = {"Logistic Regression": 0.878, "KNN": 0.902, "Decision Tree": 0.878, "Neural Network": 0.872}
-f1_dictionary = {"Logistic Regression": 0.897, "KNN": 0.914, "Decision Tree": 0.867, "Neural Network": 0.888}
+accuracy_dictionary = {"Logistic Regression": 0.880, "KNN": 0.696, "Decision Tree": 0.841, "Neural Network": 0.728}
+precision_dictionary = {"Logistic Regression": 0.917, "KNN": 0.756, "Decision Tree": 0.857, "Neural Network": 0.728}
+recall_dictionary = {"Logistic Regression": 0.878, "KNN": 0.720, "Decision Tree": 0.878, "Neural Network": 0.866}
+f1_dictionary = {"Logistic Regression": 0.897, "KNN": 0.738, "Decision Tree": 0.867, "Neural Network": 0.791}
 
 models = sorted(accuracy_dictionary.keys())
 
